812b60b4986989716ebca1b00e6c062aa2a7782b
NU-COSMOS <nucosmos1234@gmail.com>
2023-10-07 01:30:32 +0900

    新しい関数を追加

--- main.py
+++ main.py
@@ -2,26 +2,50 @@ import pandas as pd
 
 from team import Team
 
+
+def make_kill_rank(teams: list[Team], top_n: int=10) -> list[dict[str, str, int]]:
+    """合計キル数上位n人を求める"""
+    kill_rank = []
+
+    # 各チームのメンバーをフラットなリストにする
+    all_members = [m for t in teams for m in t.members]
+
+    # キル数でソート
+    all_members.sort(key=lambda m: m.total_kills, reverse=True)
+
+    # 上位n人を取得
+    top_players = all_members[:top_n]
+
+    # 辞書形式に変換
+    kill_rank = [{"Team": player.team, "Name": player.name, "Kill": player.total_kills} for player in top_players]
+
+    # for i in kill_rank:
+    #     print(i)
+    return kill_rank
+
+
 def main():
     excel_name = 'sample.xlsx'
     excel = pd.ExcelFile(excel_name)
     
     # ポイントルールの読み込み
     rule = excel.parse("Rule")
-    rank_points = {}
+    rank_pts = {}
     for _, row in rule.iterrows():
         if row["Rank"] != "kill":
-            rank_points[row["Rank"]] = row["Point"]
+            rank_pts[row["Rank"]] = row["Point"]
         else:
-            kill_point = row["Point"]
+            kill_pt = row["Point"]
 
     # 各チームデータの読み込み
     tags = excel.sheet_names
     tags.remove("Rule")
-    teams = {}
+    teams = []
     for tag in tags:
         team_sheet = excel.parse(tag)
-        teams[tag] = Team(team_sheet, tag)
+        teams.append(Team(team_sheet, tag, rank_pts, kill_pt))
+
+    make_kill_rank(teams)
 
 
 if __name__ == "__main__":
--- team.py
+++ team.py
@@ -3,7 +3,11 @@ import pandas as pd
 from member import Member
 
 class Team:
-    def __init__(self, sheet: pd.DataFrame, tag: str=None) -> None:
+    def __init__(self, sheet: pd.DataFrame, tag: str, rank_pts: dict[int], kill_pt: int) -> None:
+        # 順位ポイント
+        self.rank_pts = rank_pts
+        # キルポイント
+        self.kill_pt = kill_pt
         # チームデータのエクセルシート
         self.sheet = sheet
         # チームタグ
@@ -11,15 +15,24 @@ class Team:
         # チーム名
         self.team_name = self.sheet.columns[0]
         # チームメンバー全員のインスタンスを作成
-        self.members = self.members()
-        
+        self.members = [Member(row) for _, row in self.sheet.iterrows() if row[self.team_name] != "順位"]
+        # 合計キル数
+        self.total_kills = sum(m.total_kills for m in self.members)
+        # 順位ポイント合計
+        self.total_rank_pt = self.calc_total_rank_pt()
+        # キルポイント合計
+        self.total_kill_pt = self.total_kills * kill_pt
+        # 合計獲得ポイント
+        self.total_pt = self.total_rank_pt + self.total_kill_pt
 
-    def members(self) -> list[Member]:
-        """チームメンバーのインスタンスを作成"""
-        members = []
+    def calc_total_rank_pt(self):
+        """順位ポイントの合計"""
+        total_rank_pt = 0
+
         for _, row in self.sheet.iterrows():
             if row[self.team_name] == "順位":
-                break
-            members.append(Member(row))
-
-        return members
+                ranks = row[1:]  # チームの順位が格納されている部分
+                rank_pts = [self.rank_pts[rank] for rank in ranks if rank in self.rank_pts]
+                total_rank_pt += sum(rank_pts)
+        
+        return total_rank_pt
